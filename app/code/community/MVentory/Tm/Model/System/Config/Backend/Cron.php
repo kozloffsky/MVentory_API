af58d51e5d38d70e744a3bb8be8d38183dc392af
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-08-12 20:55:39 +0000

    (TM) Change line ending and size of indent; styling
    git-svn-id: https://mageventory.googlecode.com/svn/branches@539 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/System/Config/Backend/Cron.php
+++ app/code/community/MVentory/Tm/Model/System/Config/Backend/Cron.php
@@ -1,18 +1,18 @@
 <?php
-class MVentory_Tm_Model_System_Config_Backend_Cron extends Mage_Core_Model_Config_Data
-{
-    const CRON_PATH = 'crontab/jobs/mventory_tm_check/schedule/cron_expr';
+class MVentory_Tm_Model_System_Config_Backend_Cron
+  extends Mage_Core_Model_Config_Data {
 
-    protected function _afterSave()
-    {
-        try {
-            Mage::getModel('core/config_data')
-                ->load(self::CRON_PATH, 'path')
-                ->setValue((int)$this->getValue() ? '*/' . (int)$this->getValue() . ' * * * *' : '')
-                ->setPath(self::CRON_PATH)
-                ->save();
-        } catch (Exception $e) {
-            throw new Exception(Mage::helper('cron')->__('Unable to save the cron expression.'));
-        }
+  const CRON_PATH = 'crontab/jobs/mventory_tm_check/schedule/cron_expr';
+
+  protected function _afterSave () {
+    try {
+      Mage::getModel('core/config_data')
+        ->load(self::CRON_PATH, 'path')
+        ->setValue((int)$this->getValue() ? '*/' . (int)$this->getValue() . ' * * * *' : '')
+        ->setPath(self::CRON_PATH)
+        ->save();
+    } catch (Exception $e) {
+      throw new Exception(Mage::helper('cron')->__('Unable to save the cron expression.'));
     }
+  }
 }
