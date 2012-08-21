b2705c1c1d7ecb1151b96d379be3ecc2b10ad002
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-08-21 18:25:41 +0000

    (Tm) Implement api call to filter orders by status (issue #189)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@586 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/community/MVentory/Tm/Model/Order/Api.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Order API
+ *
+ * @category   MVentory
+ * @package    MVentory_Tm
+ * @author     MVentory <???@mventory.com>
+ */
+class MVentory_Tm_Model_Order_Api extends Mage_Sales_Model_Order_Api {
+
+  public function listByStatus ($status) {
+    $storeId = Mage::helper('mventory_tm')->getCurrentStoreId();
+
+    $collection = Mage::getModel("sales/order")
+                    ->getCollection();
+
+    try {
+      $collection
+        ->addFieldToFilter('status', 'complete')
+        ->addFieldToFilter('store_id', $storeId);
+    } catch (Mage_Core_Exception $e) {
+      $this->_fault('filters_invalid', $e->getMessage());
+    }
+
+    $orders = array();
+
+    foreach ($collection as $order) {
+      $items = $order->getAllItems();
+
+      $productsNames = array();
+
+      foreach ($items as $item)
+        $productsNames[] = array('name' => $item->getName());
+
+      $data = $order->getData();
+
+      $orders[] = array(
+        'increment_id' => $data['increment_id'],
+        'created_at' => $data['created_at'],
+        'customer_firstname' => $data['customer_firstname'],
+        'customer_lastname' => $data['customer_lastname'],
+        'customer_email' => $data['customer_email'],
+        'items' => $productsNames
+      );
+    }
+
+    return $orders;
+  }
+}
