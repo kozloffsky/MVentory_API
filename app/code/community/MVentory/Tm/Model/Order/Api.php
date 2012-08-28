ac6587dd7cd9a5955d78e082398ab8f99ddde840
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-08-28 20:11:09 +0000

    (Tm) Order by last update time; limit number of returned orders; make status parameter optional (issue #235)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@626 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Order/Api.php
+++ app/code/community/MVentory/Tm/Model/Order/Api.php
@@ -9,16 +9,26 @@
  */
 class MVentory_Tm_Model_Order_Api extends Mage_Sales_Model_Order_Api {
 
-  public function listByStatus ($status) {
+  public function listByStatus ($status = null) {
     $storeId = Mage::helper('mventory_tm')->getCurrentStoreId();
+
+    $limit = (int) Mage::getStoreConfig(
+                     MVentory_Tm_Model_Product_Api::FETCH_LIMIT_PATH,
+                     $storeId);
 
     $collection = Mage::getModel("sales/order")
                     ->getCollection();
 
     try {
+      if ($status)
+        $collection
+          ->addFieldToFilter('status', $status);
+
       $collection
-        ->addFieldToFilter('status', $status)
-        ->addFieldToFilter('store_id', $storeId);
+        ->addFieldToFilter('store_id', $storeId)
+        ->setOrder('updated_at', Varien_Data_Collection_Db::SORT_ORDER_DESC)
+        ->setPageSize($limit)
+        ->setCurPage(1);
     } catch (Mage_Core_Exception $e) {
       $this->_fault('filters_invalid', $e->getMessage());
     }
