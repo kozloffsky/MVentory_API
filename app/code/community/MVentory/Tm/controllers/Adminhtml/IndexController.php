80f653da844f0e6f717635b915308a1fe4c6de2d
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-09-25 16:08:35 +0000

    (Tm) Code reformatting; proper retrieving config values from website's scope; using our helper for translating messages
    git-svn-id: https://mageventory.googlecode.com/svn/branches@721 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/controllers/Adminhtml/IndexController.php
+++ app/code/community/MVentory/Tm/controllers/Adminhtml/IndexController.php
@@ -1,108 +1,164 @@
 <?php
 
-class MVentory_Tm_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
-{
-  public function submitAction()
-  {
+class MVentory_Tm_Adminhtml_IndexController
+  extends Mage_Adminhtml_Controller_Action {
+
+  public function submitAction () {
     $id = $this->_request->getParam('id');
     $categoryId = $this->_request->getParam('tm_category_id');
 
     $product = Mage::getModel('catalog/product')->load($id);
-    $result = 'Error';
-    
-    $stock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
-    if ($stock->getManageStock() && $stock->getQty() == 0 && !$product->getTmListingId()) {
-      Mage::getSingleton('adminhtml/session')->addError('Item is not available in inventory');
-    } else {    
-      if ($product->getId()) {
-        $connector = Mage::getModel('mventory_tm/connector');
-        $result = $connector->send($product, $categoryId);
-      }
-      
-      if (is_int($result)) {
-        $link = '<a href="http://www.' . (Mage::getStoreConfig('mventory_tm/settings/sandbox') ? 'tmsandbox' : 'trademe') . '.co.nz/Browse/Listing.aspx?id=' . $result . '">http://www.' . (Mage::getStoreConfig('mventory_tm/settings/sandbox') ? 'tmsandbox' : 'trademe') . '.co.nz/Browse/Listing.aspx?id=' . $result . '</a>';
 
-        Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('catalog')->__('Send to ') . $link);
+    $helper = Mage::helper('mventory_tm');
 
-        $product->setTmListingId($result);
-        $product->save();
-      } else {
-        Mage::getSingleton('adminhtml/session')->addError($result);
-      }
+    if (!$product->getId()) {
+      Mage::getSingleton('adminhtml/session')
+        ->addError($helper->__('Can\'t load product'));
+
+      $this->_redirect('adminhtml/catalog_product/edit/id/' . $id);
+
+      return;
     }
-    
+
+    $stock = Mage::getModel('cataloginventory/stock_item')
+               ->loadByProduct($product);
+
+    if ($stock->getManageStock() && $stock->getQty() == 0
+        && !$product->getTmListingId()) {
+      Mage::getSingleton('adminhtml/session')
+        ->addError($helper->__('Item is not available in inventory'));
+
+      $this->_redirect('adminhtml/catalog_product/edit/id/' . $id);
+
+      return;
+    }
+
+    $connector = Mage::getModel('mventory_tm/connector');
+
+    $result = $connector->send($product, $categoryId);
+
+    if (!is_int($result)) {
+      Mage::getSingleton('adminhtml/session')->addError($helper->__($result));
+
+      $this->_redirect('adminhtml/catalog_product/edit/id/' . $id);
+
+      return;
+    }
+
+    $path = MVentory_Tm_Model_Connector::SANDBOX_PATH;
+    $website = $helper->getWebsiteIdFromProduct($product);
+
+    $host = $helper->getConfig($path, $website) ? 'tmsandbox' : 'trademe';
+    $url = 'http://www.' . $host . '.co.nz/Browse/Listing.aspx?id=' . $result;
+
+    $link = '<a href="' . $url . '">' . $url . '</a>';
+
+    Mage::getSingleton('adminhtml/session')
+      ->addSuccess($helper->__('Listing URL') . ': ' . $link);
+
+    $product
+      ->setTmListingId($result)
+      ->save();
+
     $this->_redirect('adminhtml/catalog_product/edit/id/' . $id);
   }
-  
-  
-  public function removeAction()
-  {
+
+  public function removeAction () {
     $id = $this->_request->getParam('id');
     $product = Mage::getModel('catalog/product')->load($id);
     $result = 'Error';
-    
+
     if ($product->getId()) {
       $connector = Mage::getModel('mventory_tm/connector');
       $result = $connector->remove($product);
     }
-    
+
+    $helper = Mage::helper('mventory_tm');
+
     if ($result === true) {
-      $link = '<a href="http://www.' . (Mage::getStoreConfig('mventory_tm/settings/sandbox') ? 'tmsandbox' : 'trademe') . '.co.nz/Browse/Listing.aspx?id=' . $product->getTmListingId() . '">http://www.' . (Mage::getStoreConfig('mventory_tm/settings/sandbox') ? 'tmsandbox' : 'trademe') . '.co.nz/Browse/Listing.aspx?id=' . $product->getTmListingId() . '</a>';
-      
-      Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('catalog')->__('Removed from ') . $link);
-      
-      $product->setTmListingId(0);
-      $product->save();
-    } else {
-      Mage::getSingleton('adminhtml/session')->addError($result);
-    }
-    
+      $path = MVentory_Tm_Model_Connector::SANDBOX_PATH;
+      $website = $helper->getWebsiteIdFromProduct($product);
+
+      $host = $helper->getConfig($path, $website) ? 'tmsandbox' : 'trademe';
+      $url = 'http://www.'
+             . $host
+             . '.co.nz/Browse/Listing.aspx?id='
+             . $product->getTmListingId();
+
+      $link = '<a href="' . $url . '">' . $url . '</a>';
+
+      Mage::getSingleton('adminhtml/session')
+        ->addSuccess($helper->__('Removed from') . ': ' . $link);
+
+      $product
+        ->setTmListingId(0)
+        ->save();
+    } else
+      Mage::getSingleton('adminhtml/session')->addError($helper->__($result));
+
     $this->_redirect('adminhtml/catalog_product/edit/id/' . $id);
   }
-  
-  
-  public function checkAction()
-  {        
+
+  public function checkAction () {
     $id = $this->_request->getParam('id');
     $product = Mage::getModel('catalog/product')->load($id);
-    
+
+    $helper = Mage::helper('mventory_tm');
+
     if ($product->getId()) {
       $connector = Mage::getModel('mventory_tm/connector');
       $result = $connector->check($product);
-      
-      $link = '<a href="http://www.' . (Mage::getStoreConfig('mventory_tm/settings/sandbox') ? 'tmsandbox' : 'trademe') . '.co.nz/Browse/Listing.aspx?id=' . $product->getTmListingId() . '">http://www.' . (Mage::getStoreConfig('mventory_tm/settings/sandbox') ? 'tmsandbox' : 'trademe') . '.co.nz/Browse/Listing.aspx?id=' . $product->getTmListingId() . '</a>';
-      
+
+      $path = MVentory_Tm_Model_Connector::SANDBOX_PATH;
+      $website = $helper->getWebsiteIdFromProduct($product);
+
+      $host = $helper->getConfig($path, $website) ? 'tmsandbox' : 'trademe';
+      $url = 'http://www.'
+             . $host
+             . '.co.nz/Browse/Listing.aspx?id='
+             . $product->getTmListingId();
+
+      $link = '<a href="' . $url . '">' . $url . '</a>';
+
       switch ($result) {
         case 1:
-          Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('catalog')->__('Did\'t sell ') . $link);
-          
+          Mage::getSingleton('adminhtml/session')
+            ->addSuccess($helper->__('Wasn\'t sold') . ': ' . $link);
+
           $product->setTmListingId(0);
           $product->save();
+
           break;
         case 2:
-          Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('catalog')->__('Successfully sold ') . $link);
-          
-          $stock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
-    
+          Mage::getSingleton('adminhtml/session')
+            ->addSuccess($helper->__('Successfully sold ') . ': ' . $link);
+
+          $stock = Mage::getModel('cataloginventory/stock_item')
+                     ->loadByProduct($product);
+
           if ($stock->getManageStock() && $stock->getQty()) {
             $stockData = $stock->getData();
             $stockData['qty'] -= 1;
             $product->setStockData($stockData);
           }
-          
-          $product->setTmListingId(0);
-          $product->save();
+
+          $product
+            ->setTmListingId(0)
+            ->save();
+
           break;
         case 3:
-          Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('catalog')->__('The auction is active ') . $link);
+          Mage::getSingleton('adminhtml/session')
+            ->addSuccess($helper->__('Listing is active ') . ': ' . $link);
+
           break;
         default:
-          Mage::getSingleton('adminhtml/session')->addError('Listing doesn\'t exist');
+          Mage::getSingleton('adminhtml/session')
+            ->addError($helper->__('Listing doesn\'t exist'));
       }
-    } else {
-      Mage::getSingleton('adminhtml/session')->addError('Error');
-    }
-    
+    } else
+      Mage::getSingleton('adminhtml/session')->addError($helper->__('Error'));
+
     $this->_redirect('adminhtml/catalog_product/edit/id/' . $id);
   }
 }
