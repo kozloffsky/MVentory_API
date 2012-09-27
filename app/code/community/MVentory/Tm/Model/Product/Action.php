e2b893a1b8d7e199fb8521176c7a44f240a6fe00
anemets1@gmail.com <anemets1@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-09-27 11:20:20 +0000

    fix for size approximation for mass update(issue #258)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@741 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Product/Action.php
+++ app/code/community/MVentory/Tm/Model/Product/Action.php
@@ -91,101 +91,106 @@ class MVentory_Tm_Model_Product_Action extends Mage_Core_Model_Abstract {
    * @param  int|null $storeId   
    * @return int     
    */
-  public function populateAttributes ($productIds, $storeId = null) {
+  public function populateAttributes($productIds, $storeId = null) {
     $numberOfPopulatedProducts = 0;
 
     foreach ($productIds as $productId) {
-    
+
       // load product by product id
       $product = Mage::getModel('catalog/product');
-      if($storeId) $product->setStoreId($storeId);
+      if ($storeId)
+        $product->setStoreId($storeId);
       $product = $product->load($productId);
 
       $updateProduct = false;
 
-      $productData = $product->getData();
+      $productData     = $product->getData();
       $productResource = $product->getResource();
 
       // process product attributes
       foreach ($productData as $code => $value) {
-      
+
         // filter attributes which have '_' as a last symbol
-        if (substr($code, -1) == '_') {
-        
+        if (substr($code, -1) != '_')
+          continue;
+
+        // get attribute default value
+        $defaultValue = $productResource
+                          ->getAttribute($code)
+                          ->getDefaultValue();
+
+        // Example default value: 
+        // ~approx{/size_width/size_length/size_thickness}
+
+        // skip this attribute if the default value is empty or 
+        // the first symbol isn't '~' 
+        if (empty($defaultValue) || substr($defaultValue, 0, 1) != '~')
+          continue;
+
+        $parts = explode('{', $defaultValue);
+
+        // get function ('approx' for example)
+        $func = substr($parts[0], 1);
+
+        // Approximation function
+        // See issue 258 for details
+        if ($func == 'approx') {
+
+          // get delimeter ('/' for example)
+          $delim = substr($parts[1], 0, 1);
+
+          // get approximated attributes codes from default value using delimeter
+          $attributeCodes = explode($delim, substr(substr($parts[1], 0, strlen($parts[1]) - 1), 1));
+          
           // get attribute value
           $_value = $productResource
-                      ->getAttribute($code)
-                      ->getFrontend()
-                      ->getValue($product);
-                      
-          // get attribute default value
-          $defaultValue = $productResource
-                      ->getAttribute($code)
-                      ->getDefaultValue();
-
-          // Example default value: 
-          // ~approx{/size_width/size_length/size_thickness}
-
-          // skip this attribute if the default value is empty or 
-          // the first symbol isn't '~' 
-          if (empty($defaultValue)) continue;
-          if (substr($defaultValue, 0, 1) != '~') continue;
-
-          $parts = explode('{', $defaultValue);
+                  ->getAttribute($code)
+                  ->getFrontend()
+                  ->getValue($product);
           
-          // get function ('approx' for example)
-          $func = substr($parts[0], 1);
+          // get values which needed to be approximated
+          $values = explode($delim, $_value);
 
-          // Approximation function
-          // See issue 258 for details
-          if ($func == 'approx') {
-          
-            // get delimeter ('/' for example)
-            $delim = substr($parts[1], 0, 1);
+          foreach ($attributeCodes as $i => $attributeCode) {
+
+            if(!isset($values[$i]))
+              break;
             
-            // get approximated attributes codes from default value using delimeter
-            $attributeCodes = explode($delim, substr( substr($parts[1], 0, strlen($parts[1])-1), 1) );
+            // get attribute object from attribute code
+            $attribute = $productResource->getAttribute($attributeCode);
 
-            // get values which needed to be approximated
-            $values = explode($delim, $_value);
-            
-            foreach ($attributeCodes as $i => $attributeCode) {
-              
-              // get attribute object from attribute code
-              $attribute = $productResource->getAttribute($attributeCode);
+            if (!$attribute)
+              continue;
 
-              if (!$attribute) continue;
-              
-              $attributeOptions = $attribute->getSource()->getAllOptions(false);
+            $attributeOptions = $attribute->getSource()->getAllOptions(false);
 
-              // get value for this attribute that needed to be approzimated
-              $v = $values[$i];
-              
-              $min = 9999;
-              $temp = -1;
-              
-              // search the nearest attribute's option for the given value $v
-              foreach ($attributeOptions as $attributeOption) {
-                // skip non numeric options
-                if (!is_numeric($attributeOption['label'])) continue;
-                
-                if (abs($attributeOption['label'] - $v) < $min) {
-                  $min = abs($attributeOption['label'] - $v);
-                  $temp = $attributeOption;
-                } elseif (abs($attributeOption['label'] - $v) == $min &&
-                         $attributeOption['label'] > $temp['label']) {
-                  $temp = $attributeOption;
-                } 
+            // get value for this attribute that needed to be approzimated
+            $v = $values[$i];
+
+            $min  = 9999;
+            $temp = -1;
+
+            // search the nearest attribute's option for the given value $v
+            foreach ($attributeOptions as $attributeOption) {
+              // skip non numeric options
+              if (!is_numeric($attributeOption['label']))
+                continue;
+
+              if (abs($attributeOption['label'] - $v) < $min) {
+                $min  = abs($attributeOption['label'] - $v);
+                $temp = $attributeOption;
+              } elseif (abs($attributeOption['label'] - $v) == $min &&
+                      $attributeOption['label'] > $temp['label']) {
+                $temp = $attributeOption;
               }
-              
-              // check if the nearest attribute's option is found
-              if ($temp != -1) {             
-                // update value for the approximated attribute only if it's changed
-                if ($product->getData($attributeCode) != $temp['value']) {
-                  $product->setData($attributeCode, $temp['value']);
-                  $updateProduct = true;
-                }
-              }
+            }
+
+            // check if the nearest attribute's option is found and
+            // update value for the approximated attribute only if it's changed
+            if ($temp != -1 
+                && $product->getData($attributeCode) != $temp['value']) {
+                $product->setData($attributeCode, $temp['value']);
+                $updateProduct = true;
             }
           }
         }
