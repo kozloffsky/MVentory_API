c55e540ca9d6fea0408b0b543c564c2ee1ebbc86
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-10-23 09:03:22 +0000

    (Tm)  cd /home/jimcrow/programming/magento/app/code/community/MVentory/Tm/Block
    git-svn-id: https://mageventory.googlecode.com/svn/branches@850 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/community/MVentory/Tm/Block/System/Config/Form/Field/Accounts.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * TM accounts block
+ *
+ * @category   MVentory
+ * @package    MVentory_Tm
+ * @author     MVentory <???@mventory.com>
+ */
+
+class MVentory_Tm_Block_System_Config_Form_Field_Accounts
+  extends Mage_Adminhtml_Block_System_Config_Form_Field_Array_Abstract {
+
+  public function __construct() {
+    $helper = Mage::helper('mventory_tm');
+
+    $this->addColumn('name', array(
+      'label' => $helper->__('Profile name'),
+      'style' => 'width:80px',
+    ));
+
+    $this->addColumn('keys', array(
+      'label' => $helper->__('API Key'),
+      'style' => 'width:80px',
+    ));
+
+    $this->addColumn('secret', array(
+      'label' => $helper->__('API Secret'),
+      'style' => 'width:80px',
+    ));
+
+    $this->_addAfter = false;
+    $this->_addButtonLabel = $helper->__('Add account');
+
+    parent::__construct();
+  }
+}
