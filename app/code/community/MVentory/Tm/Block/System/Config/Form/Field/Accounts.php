9df3e53bed006be0710db0a8d0e082a6e73fed22
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-10-25 06:29:32 +0000

    (Tm) Fix column ID (issue #267)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@857 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Block/System/Config/Form/Field/Accounts.php
+++ app/code/community/MVentory/Tm/Block/System/Config/Form/Field/Accounts.php
@@ -18,7 +18,7 @@ class MVentory_Tm_Block_System_Config_Form_Field_Accounts
       'style' => 'width:80px',
     ));
 
-    $this->addColumn('keys', array(
+    $this->addColumn('key', array(
       'label' => $helper->__('API Key'),
       'style' => 'width:80px',
     ));
