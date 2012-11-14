e3fed21e7843a7b99952ebe1011bd90301ef6a5b
possitron <possitron@805d05ed-e12b-7278-cd1c-88623764219b>
2012-11-14 13:41:26 +0000

    #308
    git-svn-id: https://mageventory.googlecode.com/svn/branches@917 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Helper/Image.php
+++ app/code/community/MVentory/Tm/Helper/Image.php
@@ -1,6 +1,7 @@
 <?php
 
 class MVentory_Tm_Helper_Image extends Mage_Catalog_Helper_Image {
+
   public function __toString() {
     $destSubdir = $this->_getModel()->getDestinationSubdir();
 
@@ -15,6 +16,35 @@ class MVentory_Tm_Helper_Image extends Mage_Catalog_Helper_Image {
         && $bkThumbnail)
       return $bkThumbnail . '&zoom=' . ($destSubdir == 'image' ? 1 : 5);
 
-    return parent::__toString();
+    if ($this->getImageFile()) {
+      $imageFileName = $this->getImageFile();
+    } else {
+      $imageFileName = $this->getProduct()->getData($destSubdir);
+    }
+
+    if ($imageFileName == 'no_selection')
+    {
+      $imageFileName = '/no_selection';
+    }
+
+    $dimensions = $this->_getModel()->getWidth()."x".$this->_getModel()->getHeight();
+
+    if ($dimensions == "x")
+    {
+      $dimensions = "full";
+    }
+
+    $cdnBaseUrl = Mage::getBaseUrl('media');
+
+    $helper = Mage::helper('mventory_tm');
+    $website = $helper->getWebsite($this->getProduct());
+
+    $bucket = $helper->getConfig(MVentory_Tm_Model_Observer::XML_PATH_CDN_BUCKET, $website);
+    $prefix = $helper->getConfig(MVentory_Tm_Model_Observer::XML_PATH_CDN_PREFIX, $website);
+
+    $cdnPrefix = $bucket . '/' . $prefix . '/';
+    $cdnPath = $cdnPrefix . $dimensions . $imageFileName;
+
+    return $cdnBaseUrl . $cdnPath;
   }
 }
