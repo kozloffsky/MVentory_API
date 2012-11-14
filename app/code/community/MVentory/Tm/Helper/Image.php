9bb07548bbe3df5ec9e1f2660ce9fc075e7e0ea6
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-11-14 06:56:13 +0000

    (Tm) Fix indent and style
    git-svn-id: https://mageventory.googlecode.com/svn/branches@915 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Helper/Image.php
+++ app/code/community/MVentory/Tm/Helper/Image.php
@@ -1,23 +1,20 @@
 <?php
 
-class MVentory_Tm_Helper_Image extends Mage_Catalog_Helper_Image
-{
-    public function __toString()
-    {
+class MVentory_Tm_Helper_Image extends Mage_Catalog_Helper_Image {
+  public function __toString() {
+    $destSubdir = $this->_getModel()->getDestinationSubdir();
 
-      $destSubdir = $this->_getModel()->getDestinationSubdir();
+    //!!!Should be fixed. It loads full product data on every image
+    //request.
+    $bkThumbnail = Mage::getModel('catalog/product')
+                     ->load($this->getProduct()->getId())
+                     ->getData('bk_thumbnail_');
 
-      //!!!Should be fixed. It loads full product data on every image
-      //request.
-      $bkThumbnail = Mage::getModel('catalog/product')
-                       ->load($this->getProduct()->getId())
-                       ->getData('bk_thumbnail_');
+    if (in_array($destSubdir, array('image', 'small_image'))
+        && $this->getProduct()->getData($destSubdir) == 'no_selection'
+        && $bkThumbnail)
+      return $bkThumbnail . '&zoom=' . ($destSubdir == 'image' ? 1 : 5);
 
-      if (in_array($destSubdir, array('image', 'small_image'))
-          && $this->getProduct()->getData($destSubdir) == 'no_selection'
-          && $bkThumbnail)
-        return $bkThumbnail . '&zoom=' . ($destSubdir == 'image' ? 1 : 5);
-
-      return parent::__toString();
-    }
+    return parent::__toString();
+  }
 }
