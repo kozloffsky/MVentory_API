b6bae3d5eb883635873638939e76862237d6ca50
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-11-24 17:15:48 +0000

    (Tm) Use single instance of TM helper (issue #330)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@963 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Connector.php
+++ app/code/community/MVentory/Tm/Model/Connector.php
@@ -288,12 +288,14 @@ class MVentory_Tm_Model_Connector extends Mage_Core_Model_Abstract {
         return 'Product doesn\'t have matched tm category';
       }
 
+      $tmHelper = Mage::helper('mventory_tm/tm');
+
       $photoId = null;
 
       $imagePath = Mage::getBaseDir('media') . DS . 'catalog' . DS . 'product'
                      . $product->getImage();
 
-      if (!Mage::helper('mventory_tm/s3')->download($imagePath))
+      if (!$tmHelper->download($imagePath))
         return 'Downloading image from S3 failed';
 
       if (file_exists($imagePath)) {
@@ -348,8 +350,6 @@ class MVentory_Tm_Model_Connector extends Mage_Core_Model_Abstract {
         $description = $this->processDescription($descriptionTmpl, $product);
         $description = htmlspecialchars($description);
       }
-
-      $tmHelper = Mage::helper('mventory_tm/tm');
 
       //Apply fees to price of the product if it's allowed
       $price = isset($data['add_tm_fees']) && $data['add_tm_fees']
