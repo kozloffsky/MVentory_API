f02ff4def69b91e5758d2be86bb31b4673fd727b
possitron <possitron@805d05ed-e12b-7278-cd1c-88623764219b>
2013-02-03 19:00:48 +0000

    #383: Sell from a cart API
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1223 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Cart/Api.php
+++ app/code/community/MVentory/Tm/Model/Cart/Api.php
@@ -239,9 +239,265 @@ class MVentory_Tm_Model_Cart_Api extends Mage_Checkout_Model_Cart_Api {
     return $result;
   }
   
+  private function convertTo32bitTransactionId($transactionId)
+  {
+    return (int) ($transactionId % 2147483647); //max value of signed 32-bit integer
+  }
+  
+  public function createOrderForMultipleProducts ($productsToOrder) {
+
+    $orderApi = Mage::getModel('mventory_tm/order_api');
+
+    $helper = Mage::helper('mventory_tm');
+
+    $storeId = $helper->getCurrentStoreId();
+
+    $transactionId = 0;
+    
+    $productApi = Mage::getModel('mventory_tm/product_api');
+    
+    if (is_array($productsToOrder) && count($productsToOrder)>0)
+    {
+      $firstTransaction = $productsToOrder[0];
+
+      //Try load order which was created but API client didn't received
+      //its ID. It prevents from double ordering in case data lost during
+      //communication
+      $transactionId = $this->convertTo32bitTransactionId($firstTransaction['transaction_id']); 
+
+      $orderId = Mage::getResourceModel('mventory_tm/order_transaction')
+                   ->getOrderIdByTransaction($transactionId);
+
+      if ($orderId) {
+      	return $orderApi->fullInfo($orderId);
+      }
+    } else {
+        $this->_fault('invalid_params');
+    }
+    
+    foreach ($productsToOrder as &$productData)
+    {
+      $cartItem = Mage::getModel('mventory_tm/cart_item');
+
+      $cartItem->load($productData['transaction_id']);
+      
+      if (!$cartItem->getId())
+      {
+      	$this->_fault('transaction_not_exists');
+      }
+      
+      if (!isset($customerId))
+      {
+      	$customerId = $cartItem['customer_id'];
+      }	
+
+      $productData['product_id'] = $cartItem['product_id'];
+      
+      if (!isset($productData['price'])) {
+      	$productData['price'] = $cartItem['price'];
+      }
+      
+      if (!isset($productData['qty'])) {
+        $productData['qty'] = $cartItem['qty'];
+      }
+    }
+    
+    unset($productData);
+ 
+    foreach ($productsToOrder as &$productData)
+    {
+      $price = (float) $productData['price'];
+
+      $qtyIsDecimal = is_float($productData['qty']);
+
+      $qty = (float) $productData['qty'];
+
+      $updateProduct = false;
+
+      $product = Mage::getModel('catalog/product');
+
+      $productId = (int) $productData['product_id'];
+    
+      $product->load($productId);
+      
+      if (!$product->getId())
+      {
+        $this->_fault('product_not_exists');
+      }
+      
+      $productData['product_model'] = $product;
+      
+      $stockItem = $product->getStockItem();
+
+      if ($stockItem->getManageStock()) {
+        $productQty = (float) $stockItem->getQty();
+
+        $saveStockItem = false;
+
+        if ($productQty <= 0) {
+          //Mage::getModel('cataloginventory/stock_item_api')
+          //  ->update($sku, array('qty' => $qty, 'is_in_stock' => 1));
+          //$product->setStockData(array('qty' => $qty, 'is_in_stock' => true));
+
+          $productData['product_quantity_new'] = $productQty - $qty; 
+
+          $stockItem
+            ->setUseConfigManageStock(0)
+            ->setQty($qty)
+            ->setIsInStock(1);
+
+          $saveStockItem = true;
+        } else if (!$stockItem->getIsInStock()) {
+          //Change stock status if the qty is greater then zero but the product
+          //marked as out of stock, so it won't produce out of stock error
+          //on ordering.
+
+          $stockItem->setIsInStock(1);
+
+          $saveStockItem = true;
+        }
+
+        if ($qtyIsDecimal) {
+          $stockItem
+            ->setIsQtyDecimal(1);
+
+          $saveStockItem = true;
+        }
+
+        if ($saveStockItem)
+          $stockItem
+            ->save();
+      }
+
+      if ($product->getStatus()== Mage_Catalog_Model_Product_Status::STATUS_DISABLED) {
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        $product
+          ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+          ->save();
+
+        Mage::app()->setCurrentStore($storeId);
+
+        $productData['update_product'] = true;
+      }
+    }
+    
+    unset($productData);
+    
+    $quoteId = $this->create($storeId);
+    
+    $cartCustomer = Mage::getModel('checkout/cart_customer_api');
+
+    $data = array('mode' => Mage_Checkout_Model_Cart_Customer_Api::MODE_CUSTOMER,
+                  'entity_id' => $customerId);
+
+    $result = $cartCustomer->set($quoteId, $data, $storeId);
+
+    $addressId = Mage::getModel('customer/customer')
+                   ->load($customerId)
+                   ->getDefaultBillingAddress()
+                   ->getId();
+                   
+    $data = array('mode'
+                    => Mage_Checkout_Model_Cart_Customer_Api::ADDRESS_BILLING,
+                  'use_for_shipping' => true,
+                  'entity_id' => $addressId );
+                    
+    $result = $cartCustomer->setAddresses($quoteId, array($data), $storeId);
+
+    $cartProduct = Mage::getModel('checkout/cart_product_api');
+
+    $cartProductsData = array();
+                
+    foreach ($productsToOrder as $productData)
+    {
+      $data = array('product_id' => $productData['product_id'],
+                     'qty' => $productData['qty'],
+                     'custom_price' => $productData['price'] );
+
+      $cartProductsData[] = $data;
+    }
+
+    $result = $cartProduct->add($quoteId, $cartProductsData, $storeId);
+    
+    $quote = $this->_getQuote($quoteId, $storeId);
+
+    $i = 0;
+    foreach ($quote->getItemsCollection() as $item)
+    {
+      $productToOrder = $productsToOrder[$i];
+      
+      $item->setCustomPrice($productToOrder['price'])
+      ->setOriginalCustomPrice($productToOrder['price']);
+      $i++;
+    }
+
+    $quote
+      ->collectTotals()
+      ->save();
+
+    $cartShipping = Mage::getModel('checkout/cart_shipping_api');
+
+    $data = 'dummyshipping_dummyshipping';
+    
+    $result = $cartShipping->setShippingMethod($quoteId, $data, $storeId);
+
+    $cartPayment = Mage::getModel('checkout/cart_payment_api');
+
+    $data = array('method' => 'dummy', 0 => null);
+
+    $result = $cartPayment->setPaymentMethod($quoteId, $data, $storeId);
+
+    $orderId = $this->createOrder($quoteId, $storeId);
+
+    //create shipment and invoice to complete order
+    $shipment = Mage::getModel('sales/order_shipment_api');
+    $shipment->create($orderId);
+
+    $invoice = Mage::getModel('sales/order_invoice_api');
+    $invoice->create($orderId, null);
+
+    //Save transaction ID and orderId pair. So, it will return existing order
+    //to API client if it will try to create order with same transaction ID
+    //next time
+    $transaction = Mage::getModel('mventory_tm/order_transaction');
+
+    $transaction
+      ->setOrderId((int) $orderId)
+      ->setTransactionId($transactionId)
+      ->save();
+   
+    foreach ($productsToOrder as $productData)
+    {
+      if (isset($productData['update_product']) && $productData['update_product']===true)
+      {
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        $productData['product_model']
+          ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_DISABLED)
+          ->save();
+
+        Mage::app()->setCurrentStore($storeId);
+      }
+    
+      if (isset($productData['product_quantity_new']))
+        $productData['product_model']->getStockItem()
+          ->setUseConfigManageStock(0)
+          ->setQty($productQtyNew)
+          ->save();
+    }
+    
+    foreach ($productsToOrder as $productData)
+    {
+      Mage::getModel('mventory_tm/cart_item')->setId($productData['transaction_id'])->delete();
+    }
+
+    return $orderApi->fullInfo($orderId);
+  }
+  
   function addToCart($data)
   {
-  	$session = $this->_getSession();
+    $session = $this->_getSession();
     $user = $session->getUser();
     
     $data['user_name'] = $user->getFirstname() . " " . $user->getLastname();
