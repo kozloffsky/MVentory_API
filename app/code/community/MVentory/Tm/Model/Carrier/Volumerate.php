96927e2f2f3ab1f5bf6d1aaf010a6f8a720670b9
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-02-14 10:02:58 +0000

    (Tm) Add support for configurable volume attributes (issue #405)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1285 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Carrier/Volumerate.php
+++ app/code/community/MVentory/Tm/Model/Carrier/Volumerate.php
@@ -11,10 +11,18 @@ class MVentory_Tm_Model_Carrier_Volumerate
   extends Mage_Shipping_Model_Carrier_Abstract
   implements Mage_Shipping_Model_Carrier_Interface {
 
+  const WEIGHT_CONDITION = 1;
+  const VOLUME_CONDITION = 2;
+
+  const DIMENSIONS_DELIMITER = '/';
+
   protected $_code = 'volumerate';
   protected $_isFixed = true;
 
-  protected $_conditionNames = array('weight', 'volume');
+  protected $_conditions = array(
+    self::WEIGHT_CONDITION => 'weight',
+    self::VOLUME_CONDITION => 'volume'
+  );
 
   /**
    * Collect shipping rates
@@ -26,6 +34,16 @@ class MVentory_Tm_Model_Carrier_Volumerate
     if (!$this->getConfigFlag('active'))
       return false;
 
+    //Clone conditions for futher using
+    $conditions = $this->_conditions;
+
+    $attributes = $this->_getVolumeAttributes();
+
+    //Unset volume condition if volume attributes are not specified in
+    //carrier config
+    if (!$attributes)
+      unset($conditions[self::VOLUME_CONDITION]);
+
     $rate = 0;
 
     foreach ($request->getAllItems() as $item) {
@@ -35,16 +53,19 @@ class MVentory_Tm_Model_Carrier_Volumerate
 
       $request->setWeight($product->getWeight());
 
-      $volume = $product->getSizeHeight()
-                * $product->getSizeWidth()
-                * $product->getSizeDepth();
-
-      $request->setVolume($volume);
+      //Calculate volume if volume condition is allowed
+      if (isset($conditions[self::VOLUME_CONDITION]))
+        $request->setVolume($this->_calculateVolume($product, $attributes));
 
       $itemRate = 0;
 
-      foreach ($this->_conditionNames as $conditionName) {
-        $request->setConditionName($conditionName);
+      foreach ($conditions as $condition) {
+
+        //Ignore condition if its value is null
+        if ($request->getData($condition) === null)
+          continue;
+
+        $request->setConditionName($condition);
 
         $_rate = $this->getRate($request);
 
@@ -89,4 +110,71 @@ class MVentory_Tm_Model_Carrier_Volumerate
     return array('volumeweight' => $this->getConfigData('name'));
   }
 
+  /**
+   * Parse Volume attributes setting and return array of attribute codes
+   *
+   * @return array
+   */
+  protected function _getVolumeAttributes () {
+    $attributes = $this->getConfigData('volume_attributes');
+
+    if (!$attributes)
+      return;
+
+    $attributes = str_replace(', ', ',', $attributes);
+    $attributes = explode(',', $attributes);
+
+    if (!is_array($attributes))
+      return;
+
+    foreach ($attributes as $i => $attribute)
+      if (!$attribute)
+        unset($attributes[$i]);
+
+    if (!count($attributes))
+      return;
+
+    return $attributes;
+  }
+
+  /**
+   * Parse values of dimension attributes in specified product and calculate
+   * volume if value is correct
+   *
+   * @param Mage_Catalog_Model_Product $product Product
+   * @param array $attributes List of attribute codes
+   *
+   * @return numeric Calculated volume
+   */
+  protected function _calculateVolume ($product, $attributes) {
+    $volume = null;
+
+    foreach ($attributes as $attribute) {
+      $dimensions = $product->getData($attribute);
+
+      if (!$dimensions)
+        continue;
+
+      $dimensions = explode('/', $dimensions);
+
+      if (!is_array($attributes))
+        continue;
+
+      foreach ($dimensions as $i => $dimension)
+        if (!is_numeric($dimension) || $dimension == 0)
+          unset($dimensions[$i]);
+
+      if (count($dimensions) != 3)
+        continue;
+
+      $volume = 1;
+
+      foreach ($dimensions as $dimension)
+        $volume *= $dimension;
+
+      break;
+    }
+
+    return $volume;
+  }
 }
