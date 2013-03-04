77307af31856fffe00b2231e2539a3fd5ff19e30
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-03-04 14:24:23 +0000

    (Tm) Simplify code; use default code then category is anchor (issue #333)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1370 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Category.php
+++ app/code/community/MVentory/Tm/Model/Category.php
@@ -2,46 +2,32 @@
 
 class MVentory_Tm_Model_Category extends Mage_Catalog_Model_Category {
 
+  const DISPLAY_DESCENDING_PRODUCTS
+    = 'mventory_tm/shop-interface/display-descending-products';
+
   /**
    * Get category products collection
+   *
+   * The method is redefined to load all descending products when it's allowed
+   * and category is not anchor (anchor categories load descending products
+   * by default)
    *
    * @return Varien_Data_Collection_Db
    */
   public function getProductCollection () {
-    $displayDescendingProducts = (bool) Mage::getStoreConfig(
-            'mventory_tm/shop-interface/display-descending-products');
+    if ($this->getIsAnchor()
+        || !Mage::getStoreConfig(self::DISPLAY_DESCENDING_PRODUCTS))
+      return parent::getProductCollection();
 
-    if ($displayDescendingProducts) {
-      $ids = array($this->getId());
-
-      $sub = Mage::getModel('catalog/category')->getCategories($this->getId());
-      foreach ($sub as $cat) {
-        $ids[] = $cat->getId();
-        $sub2 = Mage::getModel('catalog/category')->getCategories($cat->getId());
-        foreach ($sub2 as $cat2) {
-          $ids[] = $cat2->getId();
-        }
-      }
-
-      $products = array();
-
-      foreach ($ids as $id) {
-        $collection = Mage::getResourceModel('catalog/product_collection')
-          ->joinField('category_id', 'catalog/category_product', 'category_id', 'product_id = entity_id', null, 'left')
-          ->addAttributeToFilter('category_id', $id)
-          ->setStoreId($this->getStoreId());
-
-        $products = array_merge($products, $collection->getAllIds());
-      }
-
-      $collection = Mage::getResourceModel('catalog/product_collection')
-        ->addAttributeToFilter('entity_id', array('in' => $products))
-        ->setStoreId($this->getStoreId());
-    } else {
-      $collection = Mage::getResourceModel('catalog/product_collection')
-        ->setStoreId($this->getStoreId())
-        ->addCategoryFilter($this);
-    }
-    return $collection;
+    return Mage::getResourceModel('catalog/product_collection')
+             ->joinField('category_id',
+                         'catalog/category_product',
+                         'category_id',
+                         'product_id = entity_id',
+                         null,
+                         'left')
+             ->addAttributeToFilter('category_id',
+                                    array('in' => $this->getAllChildren(true)))
+             ->setStoreId($this->getStoreId());
   }
 }
