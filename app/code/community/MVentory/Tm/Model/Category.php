55f0306fbfe29ac62e0aab58aa45f204db584f8f
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-03-04 14:36:57 +0000

    (Tm) Rebuild category tree, show only categories with more then 1 sub-category (issue #333)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1371 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Category.php
+++ app/code/community/MVentory/Tm/Model/Category.php
@@ -30,4 +30,55 @@ class MVentory_Tm_Model_Category extends Mage_Catalog_Model_Category {
                                     array('in' => $this->getAllChildren(true)))
              ->setStoreId($this->getStoreId());
   }
+
+  /**
+   * Retrieve categories by parent
+   *
+   * The method is redefined to show categories with more
+   * then 1 subcategory only
+   *
+   * @param int $parent
+   * @param int $recursionLevel
+   * @param bool $sorted
+   * @param bool $asCollection
+   * @param bool $toLoad
+   * @return mixed
+   */
+  public function getCategories($parent,
+                                $recursionLevel = 0,
+                                $sorted = false,
+                                $asCollection = false,
+                                $toLoad = true) {
+
+    $categories = parent::getCategories($parent,
+                                        $recursionLevel,
+                                        $sorted,
+                                        $asCollection,
+                                        $toLoad);
+
+    return $this->_removeParentCategories($categories);
+  }
+
+  /**
+   * Walk down categories tree and return first category with more
+   * than one sub-category
+   *
+   * @param int $parent
+   * @param int $recursionLevel
+   * @param bool $sorted
+   * @param bool $asCollection
+   * @param bool $toLoad
+   * @return mixed
+   */
+  protected function _removeParentCategories ($categories) {
+    if (count($nodes = $categories->getNodes()) != 1)
+      return $categories;
+
+    $children = end($nodes)->getChildren();
+
+    if (!count($children->getNodes()))
+      return $categories;
+
+    return $this->_removeParentCategories($children);
+  }
 }
