6ae2dc3c0084d1d31e802daa91fca6ab0b37548b
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-03-05 17:12:20 +0000

    (Tm) Add additional methods to SKU generator (issue #417)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1376 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/controllers/QrController.php
+++ app/code/community/MVentory/Tm/controllers/QrController.php
@@ -20,7 +20,7 @@ class MVentory_Tm_QrController extends Mage_Core_Controller_Front_Action
   }
 
 
-  public function labelsAction()
+  protected function _generateLabels ()
   {
     $size = Mage::getStoreConfig('mventory_tm/qr/size');
     $rows = Mage::getStoreConfig('mventory_tm/qr/rows');
@@ -109,6 +109,78 @@ class MVentory_Tm_QrController extends Mage_Core_Controller_Front_Action
     echo $html;
   }
 
+  public function generateAction () {
+    $request = $this->getRequest();
+
+    if ($request->getParam('labels')) {
+      $this->_generateLabels();
+
+      return;
+    }
+
+    $links = (bool) $request->getParam('links');
+    $images = (bool) $request->getParam('images');
+    $codes = (bool) $request->getParam('codes');
+
+    if (!($links || $images || $codes))
+      return;
+
+    $content = $this->_generateData($links, $images, $codes);
+
+    $this->_prepareDownloadResponse('images-link-and-codes.csv', $content);
+  }
+
+  protected function _generateData ($links = false,
+                                    $images = false,
+                                    $codes = false) {
+
+    $size = Mage::getStoreConfig('mventory_tm/qr/size');
+    $pages = (int) Mage::getStoreConfig('mventory_tm/qr/pages');
+    $rows = (int) Mage::getStoreConfig('mventory_tm/qr/rows');
+    $columns = (int) Mage::getStoreConfig('mventory_tm/qr/columns');
+    $baseUrl = Mage::getStoreConfig('mventory_tm/qr/base_url')
+                 ? Mage::getStoreConfig('mventory_tm/qr/base_url')
+                   : Mage::getBaseUrl();
+
+    if (preg_match("#https?://#", $baseUrl) === 0)
+      $baseUrl = 'http://' . $baseUrl;
+
+    if (strrpos($baseUrl, '/') + 1 != strlen($baseUrl))
+      $baseUrl .= '/';
+
+    $baseUrl .= 'sku/';
+
+    $n = $pages * $rows * $columns;
+
+    $imageUrl = 'https://chart.googleapis.com/chart?cht=qr&chs='
+                . $size
+                . 'x'
+                . $size
+                . '&chl=';
+
+    $commaAfterImage = ($links || $codes) ? ',' : '';
+    $commaAfterLink = $codes ? ',' : '';
+
+    $content = '';
+
+    for ($i = 0; $i < $n; $i++) {
+      $code = $this->_generateSku();
+      $url = $baseUrl . $code;
+
+      if ($images)
+        $content .= $imageUrl . urlencode($url) . $commaAfterImage;
+
+      if ($links)
+        $content .= $url . $commaAfterLink;
+
+      if ($codes)
+        $content .= $code;
+
+      $content .= "\r\n";
+    }
+
+    return $content;
+  }
 
   private function _generateSku()
   {
