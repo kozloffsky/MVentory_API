9c1de5eb99222d95c6072ad5e602ea65fb0cf29c
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-04-17 06:13:45 +0000

    (Tm) Send true value instead 1
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1524 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Connector.php
+++ app/code/community/MVentory/Tm/Model/Connector.php
@@ -332,7 +332,7 @@ class MVentory_Tm_Model_Connector extends Mage_Core_Model_Abstract {
       $xml .= '<ShippingOptions>
 <ShippingOption><Type>' . $shippingType . '</Type></ShippingOption>
 </ShippingOptions>
-<SendPaymentInstructions>1</SendPaymentInstructions>
+<SendPaymentInstructions>true</SendPaymentInstructions>
 <PaymentMethods>
 <PaymentMethod>CreditCard</PaymentMethod>
 <PaymentMethod>Cash</PaymentMethod>
