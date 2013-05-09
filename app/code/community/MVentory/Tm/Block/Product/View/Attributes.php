b2b8364b8009b9d99de20130a012bd226a474d69
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-05-09 07:16:02 +0000

    (Tm) Issue #493: filter out empty, n/a and ~ values; convert sku to link to QR code; take data of creation from product if mv_created_data attr is empty
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1627 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Block/Product/View/Attributes.php
+++ app/code/community/MVentory/Tm/Block/Product/View/Attributes.php
@@ -9,33 +9,6 @@
  */
 class MVentory_Tm_Block_Product_View_Attributes
  extends Mage_Catalog_Block_Product_View_Attributes {
-    protected $_product = null;
-
-  /**
-   * $excludeAttr is optional array of attribute codes to
-   * exclude them from additional data array
-   *
-   * @param array $excludeAttr
-   * @param bool $html Enable or disable links to category filters
-   * @return array
-   */
-  public function getAdditionalData (array $exclude = array(), $html = true) {
-    $data = $html
-              ? $this->getAdditionalDataHtml($exclude)
-                : parent::getAdditionalData($exclude);
-
-    $data[] = array(
-      'label' => 'Product ID',
-      'value' => $this->getProduct()->getId(),
-      'code' => 'id'
-    );
-
-    //Round value of weight attribute
-    if (isset($data['weight']) && is_numeric($data['weight']['value']))
-      $data['weight']['value'] = round($data['weight']['value'], 2);
-
-    return $data;
-  }
 
   /**
    * Outputs options of attributes with select or multiselect input type as
@@ -45,14 +18,20 @@ class MVentory_Tm_Block_Product_View_Attributes
    * exclude them from additional data array
    *
    * @param array $excludeAttr
+   * @param bool $html Enable or disable links to category filters
    * @return array
    */
-  public function getAdditionalDataHtml (array $exclude = array()) {
+  public function getAdditionalData (array $exclude = array(), $html = true) {
     $helper = Mage::helper('mventory_tm/product');
 
     $data = array();
 
     $product = $this->getProduct();
+
+    if ($html)
+      if (!$product->getData('mv_created_date'))
+        $product->setData('mv_created_date', $product->getData('created_at'));
+
     $category = $helper->getCategory($product);
     $attributes = $product->getAttributes();
 
@@ -65,58 +44,80 @@ class MVentory_Tm_Block_Product_View_Attributes
     foreach ($attributes as $attribute) {
       $code = $attribute->getAttributeCode();
 
-      if (!$product->hasData($code))
-        continue;
-
       if (!$attribute->getIsVisibleOnFront() || in_array($code, $exclude))
         continue;
 
-      $input = $attribute->getFrontendInput();
+      $label = trim($attribute->getStoreLabel());
 
-      if ($attribute->getIsHtmlAllowedOnFront()
-          && ($input == 'select' || $input == 'multiselect')) {
+      if ($label == '~')
+        continue;
+
+      $values = $product->getData($code);
+
+      if ($values === null || $values === '')
+        continue;
+
+      $input = $attribute->getFrontendInput();
+      $isSelect = $input == 'select' || $input == 'multiselect';
+
+      if ($isSelect) {
+        $values = explode(',', $values);
 
         $source = $attribute->getSource();
 
-        $links = array();
+        foreach ($values as $value)
+          $_values[$value] = $source->getOptionText($value);
 
-        $_values = explode(',', $product->getData($code));
+        $values = $_values;
 
-        foreach ($_values as $_value) {
+        unset($_values);
+      } else
+        $values = (array) $attribute
+                            ->getFrontend()
+                            ->getValue($product);
+
+      foreach ($values as $i => $value) {
+        $_value = strtolower(str_replace(' ', '', $value));
+
+        if (!$_value || $_value == 'n/a' || $_value == 'n\a' || $_value == '~')
+          unset($values[$i]);
+      }
+
+      if (!count($values))
+        continue;
+
+      if ($attribute->getIsHtmlAllowedOnFront() && $isSelect && $html) {
+        foreach ($values as $i => &$value) {
           $params = $queryParams;
-          $params[$code] = $_value;
-
-          $category->unsetData('url');
+          $params[$code] = $i;
 
           $category
+            ->unsetData('url')
             ->getUrlInstance()
             ->unsetData('query_params')
             ->setQueryParams($params);
 
-          $links[] = '<a href="' . $category->getUrl() . '"'
-                      . 'title="' . $title . '">'
-                    . $source->getOptionText($_value)
+          $value = '<a href="' . $category->getUrl() . '"'
+                    . 'title="' . $title . '">'
+                    . $value
                     . '</a>';
         }
 
-        $value = implode(', ', $links);
-      } else {
-        $value = $attribute
-                   ->getFrontend()
-                   ->getValue($product);
-
-        if ((string) $value == '')
-          $value = Mage::helper('catalog')->__('No');
-        elseif ($input == 'price' && is_string($value))
-          $value = Mage::app()->getStore()->convertPrice($value, true);
+        unset($value);
       }
 
-      if (is_string($value) && strlen($value))
-        $data[$code] = array(
-          'label' => $attribute->getStoreLabel(),
-          'value' => $value,
-          'code'  => $code
-        );
+      if ($input == 'price') {
+        foreach ($values as $i => &$value)
+          $value = Mage::app()->getStore()->convertPrice($value, true);
+
+        unset($value);
+      }
+
+      $data[$code] = array(
+        'label' => $label,
+        'value' => implode(', ', $values),
+        'code'  => $code
+      );
     }
 
     $category
@@ -124,6 +125,27 @@ class MVentory_Tm_Block_Product_View_Attributes
       ->getUrlInstance()
       ->unsetData('query_params')
       ->setQueryParams($queryParams);
+
+    $data[] = array(
+      'label' => 'Product ID',
+      'value' => $this->getProduct()->getId(),
+      'code' => 'id'
+    );
+
+    //Round value of weight attribute
+    if (isset($data['weight']) && is_numeric($data['weight']['value']))
+      $data['weight']['value'] = round($data['weight']['value'], 2);
+
+    if ($html) {
+      if (isset($data['sku'])) {
+        $url = $this->getUrl('', array('sku' => $data['sku']['value']));
+        $qrUrl = 'https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl='
+                 . urlencode($url); 
+
+        $data['sku']['value']
+          = '<a href="' . $qrUrl . '">' . $data['sku']['value'] . '</a>';
+      }
+    }
 
     return $data;
   }
