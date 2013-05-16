24ad0b373deb4aa861815959b9cf13ac14315ca8
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-05-16 12:21:42 +0000

    (Tm) Set size of QR code to 200x200; remove slash from the end of the product URL
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1656 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Block/Product/View/Attributes.php
+++ app/code/community/MVentory/Tm/Block/Product/View/Attributes.php
@@ -139,8 +139,11 @@ class MVentory_Tm_Block_Product_View_Attributes
     if ($html) {
       if (isset($data['sku'])) {
         $url = $this->getUrl('', array('sku' => $data['sku']['value']));
-        $qrUrl = 'https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl='
-                 . urlencode($url); 
+
+        
+
+        $qrUrl = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl='
+                 . urlencode(substr($url, 0, -1)); 
 
         $data['sku']['value']
           = '<a href="' . $qrUrl . '">' . $data['sku']['value'] . '</a>';
