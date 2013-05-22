c7fd6aadeeeea6a8c5affe55d5a237419d3795b8
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-05-22 09:20:35 +0000

    (Tm) Issue #468: add support for external home URL in breadcrumbs; make current category as link in breadcrumbs
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1689 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/community/MVentory/Tm/Helper/CatalogData.php
@@ -0,0 +1,41 @@
+<?php
+
+class MVentory_Tm_Helper_CatalogData extends Mage_Catalog_Helper_Data {
+
+  /**
+   * Same as Mage_Catalog_Helper_Data::getBreadcrumbPath(), but returns
+   * current category as link
+   *
+   * @return array
+   */
+  public function getBreadcrumbPath () {
+    if ($this->_categoryPath)
+      return $this->_categoryPath;
+
+    $path = array();
+
+    if ($category = $this->getCategory()) {
+      $pathInStore = $category->getPathInStore();
+      $pathIds = array_reverse(explode(',', $pathInStore));
+
+      $categories = $category->getParentCategories();
+
+      //Add category path breadcrumb
+      foreach ($pathIds as $categoryId)
+        if (isset($categories[$categoryId])
+            && ($name = $categories[$categoryId]->getName()))
+
+          $path['category' . $categoryId] = array(
+            'label' => $name,
+            'link' => $categories[$categoryId]->getUrl()
+          );
+    }
+
+    if ($product = $this->getProduct())
+      $path['product'] = array('label' => $product->getName());
+
+    $this->_categoryPath = $path;
+
+    return $this->_categoryPath;
+  }
+}
