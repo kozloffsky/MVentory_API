3a80d91e0160ac6c5dc23b76d3778f646898a1b7
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-06-10 08:16:09 +0000

    Issue #535: re-format and simplify code
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1713 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/community/MVentory/Tm/Model/Layer/Filter/Attribute.php
+++ app/code/community/MVentory/Tm/Model/Layer/Filter/Attribute.php
@@ -1,44 +1,37 @@
 <?php
 
-class MVentory_Tm_Model_Layer_Filter_Attribute extends Mage_Catalog_Model_Layer_Filter_Attribute
-{
-  private function cmp_items($item_left, $item_right)
-    {
-        $value_left = $item_left['label'];
-        $value_right = $item_right['label'];
+class MVentory_Tm_Model_Layer_Filter_Attribute
+  extends Mage_Catalog_Model_Layer_Filter_Attribute {
 
-        if (is_numeric($value_left) && is_numeric($value_right))
-        {
-            if (0.0 + $value_left < 0.0 + $value_right)
-                return -1;
-            elseif (0.0 + $value_left == 0.0 + $value_right)
-                return 0;
-            else
-                return 1;
-        }
-        elseif (is_numeric($value_left) && !is_numeric($value_right))
-        {
-            return -1;
-        }
-        elseif (!is_numeric($value_left) && is_numeric($value_right))
-        {
-            return 1;
-        }
-        else
-        {
-            return strcmp($value_left, $value_right);
-        }
+  protected function _compareLabels ($left, $right) {
+    $left = $left['label'];
+    $right = $right['label'];
+
+    $leftNotNumeric = !is_numeric($left);
+    $rightNotNumeric = !is_numeric($right);
+
+    if ($leftNotNumeric && $rightNotNumeric)
+      return strcmp($left, $right);
+
+    if (!($leftNotNumeric || $rightNotNumeric)) {
+      $left = (float) $left;
+      $right = (float) $right;
+
+      return $left > $right ? 1 : ($left == $right ? 0 : -1);
     }
+
+    return $leftNotNumeric - $rightNotNumeric;
+  }
 
   /**
    * Get data array for building attribute filter items
    *
    * @return array
    */
-  protected function _getItemsData()
-  {
+  protected function _getItemsData () {
     $data = parent::_getItemsData();
-    usort($data, array($this, "cmp_items"));
+
+    usort($data, array($this, '_compareLabels'));
 
     return $data;
   }
