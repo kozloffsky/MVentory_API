31d52724e8858f882dd202a657fa6ef8ba282a8a
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-12-06 05:10:43 +0000

    (Tm) Add Powered By line to the footer
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1969 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/community/MVentory/Tm/Block/Html/Footer.php
@@ -0,0 +1,11 @@
+<?php
+
+class MVentory_Tm_Block_Html_Footer extends Mage_Page_Block_Html_Footer {
+  public function getCopyright() {
+    return parent::getCopyright()
+      . '<br />'
+      . '<a id="mv-powered-by" href="//mventory.com">Powered by mVentory</a>';
+  }
+}
+
+?>
