7486ceffbc25d4e0b64a3fbb21a1f7b2c5dae034
Anatoly A. Kazantsev <anatoly@zetaprints.com>
2014-03-03 13:18:26 +0800

    Add block to rewrite rules; remove new line tag

--- app/code/community/MVentory/Tm/Block/Html/Footer.php
+++ app/code/community/MVentory/Tm/Block/Html/Footer.php
@@ -3,7 +3,6 @@
 class MVentory_Tm_Block_Html_Footer extends Mage_Page_Block_Html_Footer {
   public function getCopyright() {
     return parent::getCopyright()
-      . '<br />'
       . '<a id="mv-powered-by" href="//mventory.com">Powered by mVentory</a>';
   }
 }
