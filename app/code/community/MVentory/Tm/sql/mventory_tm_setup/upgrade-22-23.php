32459cc8025a9a5e56def78683261f95224b38ac
Anatoly A. Kazantsev <anatoly@zetaprints.com>
2014-04-07 21:58:42 +0800

    Create barcode attribute on install

---
+++ app/code/community/MVentory/Tm/sql/mventory_tm_setup/upgrade-22-23.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License BY-NC-ND.
+ * NonCommercial — You may not use the material for commercial purposes.
+ * NoDerivatives — If you remix, transform, or build upon the material,
+ * you may not distribute the modified material.
+ * See the full license at http://creativecommons.org/licenses/by-nc-nd/4.0/
+ *
+ * See http://mventory.com/legal/licensing/ for other licensing options.
+ *
+ * @package MVentory/TM
+ * @copyright Copyright (c) 2014 mVentory Ltd. (http://mventory.com)
+ * @license http://creativecommons.org/licenses/by-nc-nd/4.0/
+ * @author Anatoly A. Kazantsev <anatoly@mventory.com>
+ */
+
+$this->startSetup();
+
+$name = 'product_barcode_';
+$entityTypeId = $this->getEntityTypeId('catalog_product');
+
+$this->addAttribute(
+  $entityTypeId,
+  $name,
+  array(
+    //Fields from Mage_Eav_Model_Entity_Setup
+    'type' => 'varchar',
+    'label' => 'Barcode',
+    'required' => false,
+    'user_defined' => true,
+
+    //Fields from Mage_Catalog_Model_Resource_Setup
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'is_configurable' => false
+  )
+);
+
+$setId = $this->getDefaultAttributeSetId($entityTypeId);
+$groupId = $this->getDefaultAttributeGroupId($entityTypeId, $setId);
+
+$this->addAttributeToGroup($entityTypeId, $setId, $groupId, $name);
+
+$this->endSetup();
