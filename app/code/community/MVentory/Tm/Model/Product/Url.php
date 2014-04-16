cc76c20953942c3b353023a9fa3ed9630c6add67
Anatoly A. Kazantsev <anatoly@zetaprints.com>
2014-04-16 09:24:07 +0800

    Update for M. 1.8

--- app/code/community/MVentory/Tm/Model/Product/Url.php
+++ app/code/community/MVentory/Tm/Model/Product/Url.php
@@ -24,85 +24,26 @@
  */
 class MVentory_Tm_Model_Product_Url extends Mage_Catalog_Model_Product_Url {
 
-    /**
-     * Retrieve Product URL using UrlDataObject
-     *
-     * @param Mage_Catalog_Model_Product $product
-     * @param array $params
-     * @return string
-     */
-    public function getUrl(Mage_Catalog_Model_Product $product, $params = array()) {
-        $routePath      = '';
-        $routeParams    = $params;
+  /**
+   * Check product category
+   *
+   * The method is redifined to use first category the product is assigned
+   * if there's no current category, i.e. generating URL in product editor
+   * in the admin interface or in list of latest products on homepage.
+   *
+   * @param Mage_Catalog_Model_Product $product
+   * @param array $params
+   *
+   * @return int|null
+   */
+  protected function _getCategoryIdForUrl ($product, $params) {
+    $id = parent::_getCategoryIdForUrl($product, $params);
 
-        $storeId    = $product->getStoreId();
-        if (isset($params['_ignore_category'])) {
-            unset($params['_ignore_category']);
-            $categoryId = null;
-        } else {
-            $categoryId = $product->getCategoryId() && !$product->getDoNotUseCategoryId()
-                ? $product->getCategoryId() : null;
+    if (isset($params['_ignore_category']) || $id !== null)
+      return $id;
 
-            if (!$categoryId) {
-              $categories = $product->getCategoryIds();
+    $categories = $product->getCategoryIds();
 
-              if (count($categories)) {
-                $categoryId = $categories[0];
-
-                //Reset rewrite rules for the product
-                //We will generate new URL further
-                $product->setRequestPath(null);
-              }
-            }
-        }
-
-        if ($product->hasUrlDataObject()) {
-            $requestPath = $product->getUrlDataObject()->getUrlRewrite();
-            $routeParams['_store'] = $product->getUrlDataObject()->getStoreId();
-        } else {
-            $requestPath = $product->getRequestPath();
-            if (empty($requestPath) && $requestPath !== false) {
-                $idPath = sprintf('product/%d', $product->getEntityId());
-                if ($categoryId) {
-                    $idPath = sprintf('%s/%d', $idPath, $categoryId);
-                }
-                $rewrite = $this->getUrlRewrite();
-                $rewrite->setStoreId($storeId)
-                    ->loadByIdPath($idPath);
-                if ($rewrite->getId()) {
-                    $requestPath = $rewrite->getRequestPath();
-                    $product->setRequestPath($requestPath);
-                } else {
-                    $product->setRequestPath(false);
-                }
-            }
-        }
-
-        if (isset($routeParams['_store'])) {
-            $storeId = Mage::app()->getStore($routeParams['_store'])->getId();
-        }
-
-        if ($storeId != Mage::app()->getStore()->getId()) {
-            $routeParams['_store_to_url'] = true;
-        }
-
-        if (!empty($requestPath)) {
-            $routeParams['_direct'] = $requestPath;
-        } else {
-            $routePath = 'catalog/product/view';
-            $routeParams['id']  = $product->getId();
-            $routeParams['s']   = $product->getUrlKey();
-            if ($categoryId) {
-                $routeParams['category'] = $categoryId;
-            }
-        }
-
-        // reset cached URL instance GET query params
-        if (!isset($routeParams['_query'])) {
-            $routeParams['_query'] = array();
-        }
-
-        return $this->getUrlInstance()->setStore($storeId)
-            ->getUrl($routePath, $routeParams);
-    }
+    return isset($categories[0]) ? $categories[0] : null;
+  }
 }
