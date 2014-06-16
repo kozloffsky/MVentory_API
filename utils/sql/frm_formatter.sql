5f9fbdd82e552a20154e4e264fa27dc0831f7b44
mx <max@onebro.me>
2014-06-16 20:04:27 +1200

    Tidied up frm_formatter.sql

--- utils/sql/frm_formatter.sql
+++ utils/sql/frm_formatter.sql
@@ -1,7 +1,17 @@
--- This script inserts ~ into some or all frm_ attributes to hide them from one or more stores.
+-- This script disables all but one attribute sets for a single store by inserting ~ as the label in frm_ attributes
 
-set @attribute_set = 'audio';
-set @store = 'toolbox';
-set @store_id = (select store_id from core_store where code = @store);
-delete from l using eav_attribute_label as l, eav_attribute as a where l.attribute_id = a.attribute_id and a.attribute_code like "frm_%" and l.store_id = @store_id;
-insert into eav_attribute_label (attribute_id, store_id, value) select a.attribute_id, @store_id, '~' from eav_attribute as a where a.attribute_code like "frm_%" and a.attribute_code != CONCAT('frm_', @attribute_set);
+set @attribute = 'frm_book'; -- name of the formatting attribute to enable
+set @store = 'welovebooks'; -- the store code for the store in question
+set @store_id = (select store_id from core_store where code = @store); -- get store id for the store code
+
+-- delete all labels from all frm_ attributes for the store in question
+delete from l using eav_attribute_label as l, eav_attribute as a 
+	where l.attribute_id = a.attribute_id 
+		and a.attribute_code like "frm_%" 
+		and l.store_id = @store_id;
+
+-- re-insert ~ into all frm_ attributes for this store, except for the one that should remain enabled
+insert into eav_attribute_label (attribute_id, store_id, value) 
+	select a.attribute_id, @store_id, '~' from eav_attribute as a 
+		where a.attribute_code like "frm_%" 
+			and a.attribute_code != @attribute;
